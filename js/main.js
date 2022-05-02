b21181231a303735bb0a37007dc9b7a446029cb2
Phil Housley <undeconstructed@gmail.com>
2022-05-02 17:13:24 +0100

    Can do something

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ function getTemplate(selector) {
   }
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+function roll(n) {
+  return getRandomIntInclusive(1, n)
+}
+
 function newUI(data) {
   let state = {
   }
@@ -102,7 +112,11 @@ function makeStoryBox(_data, _up) {
       for (; seen < s.events.length; seen++) {
         let ev = s.events[seen]
         let ediv = eventTmpl()
-        ediv.textContent = `Je ${ev.time}, ${ev.player.name}, faras ${ev.name}.`
+        if (ev.text) {
+          ediv.textContent = `Je ${ev.time}, ${ev.text}`
+        } else {
+          ediv.textContent = `Je ${ev.time}, ${ev.player.name}, faras ${ev.name}.`
+        }
         list.append(ediv)
         ediv.scrollIntoView()
       }
@@ -113,40 +127,152 @@ function makeStoryBox(_data, _up) {
 }
 
 function makeNewPlayerBox(_data, up) {
-  let onCommand = c => {
-    if (c.do === 'makeplayer') {
-      let m = prompt('Nomo:')
-      if (!m) {
+  let div = document.querySelector('.newplayer')
+  let form = div.querySelector('form')
+  let nameBox = form.querySelector('[name=name]')
+
+  let state = null
+
+  ;(() => {
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      let name = nameBox.value
+      if (!name) {
         return
       }
       up.send({
         do: 'addplayer',
-        name: m,
+        name: name,
       })
+      close()
+    })
+    form.querySelector('[name=cancel]').addEventListener('click', e => {
+      e.preventDefault()
+      close()
+    })
+  })()
+
+  let setup = c => {
+    state = c
+    div.classList.add('open')
+  }
+
+  let close = () => {
+    div.classList.remove('open')
+    state = null
+    form.reset()
+  }
+
+  let onCommand = c => {
+    if (c.do === 'makeplayer') {
+      setup(c)
     }
   }
 
   return { onCommand }
 }
 
-function makeDoSomethingBox(_data, up) {
-  let onCommand = c => {
-    if (c.do === 'makeaction') {
-      let skill = c.skill
-      let m = prompt(`Ago de ${c.player.name} per '${skill.name}' je nivelo ${skill.level}:`)
-      if (!m) {
+function makeMakeActionBox(_data, up) {
+  let div = document.querySelector('.makeaction')
+  let form = div.querySelector('form')
+  let nameBox = form.querySelector('[name=name]')
+
+  let state = null
+
+  ;(() => {
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      let name = nameBox.value
+      if (!name) {
         return
       }
-      up.send({
-        do: 'action',
-        player: c.player,
-        skill: skill,
-        name: m,
-      })
+      doTry(name)
+      close()
+    })
+    form.querySelector('[name=cancel]').addEventListener('click', e => {
+      e.preventDefault()
+      close()
+    })
+  })()
+
+  let doTry = name => {
+    up.send({
+      do: 'tryaction',
+      player: state.player,
+      skill: state.skill,
+      name: name,
+    })
+  }
+
+  let setup = c => {
+    state = c
+    let m = `Ago de ${state.player.name} per '${state.skill.name}' je nivelo ${state.skill.level}:`
+    form.querySelector('.status').textContent = m
+    div.classList.add('open')
+  }
+
+  let close = () => {
+    div.classList.remove('open')
+    state = null
+    form.reset()
+  }
+
+  let onCommand = c => {
+    if (c.do === 'makeaction') {
+      setup(c)
     }
   }
 
   return { onCommand }
+}
+
+function makeTryActionBox(_data, up) {
+  let div = document.querySelector('.tryaction')
+  let result = div.querySelector('.result')
+  let form = div.querySelector('form')
+
+  ;(() => {
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      let next = form.querySelector('[name=next]').value
+      if (!next) {
+        return
+      }
+      doFinish(next)
+      close()
+    })
+  })()
+
+  let doFinish = (text) => {
+    up.send({
+      do: 'finishaction',
+      text: text,
+    })
+  }
+
+  let onUpdate = s => {
+    if (s.attempt) {
+      let attempt = s.attempt
+      div.classList.add('open')
+
+      let m = `${attempt.action.player.name} provas ${attempt.action.name}, per '${attempt.action.skill.name}' je nivelo ${attempt.action.skill.level}`
+      result.querySelector('.status').textContent = m
+
+      result.querySelector('.for').textContent = attempt.result.diceFor
+      result.querySelector('.against').textContent = attempt.result.diceAgainst
+
+      let next = form.querySelector('[name=next]')
+      if (attempt.result.success) {
+        next.value = `${attempt.action.player.name} sukcese faras ${attempt.action.name}, kaj ...`
+      } else {
+        next.value = `${attempt.action.player.name} sensukcese provas ${attempt.action.name}.`
+      }
+    } else {
+      div.classList.remove('open')
+    }
+  }
+
+  return { onUpdate }
 }
 
 // there is no server, so this does it.
@@ -154,6 +280,7 @@ function makeKernel() {
   let state = {
     players: [],
     time: 0,
+    attempt: null,
     events: [],
   }
 
@@ -163,26 +290,65 @@ function makeKernel() {
     sendUpdate = f
   }
 
+  let doAddPlayer = c => {
+    state.players.push({
+      name: c.name,
+      skills: [
+        { name: 'fari ion', level: 1 },
+      ],
+    })
+    sendUpdate(state)
+  }
+
+  let multiRoll = n => {
+    let sum = 0
+    let str = ''
+    for (let i = 0; i < n; i++) {
+      let v = roll(6)
+      sum += v
+      str += v + ' '
+    }
+    return [sum, str]
+  }
+
+  let doTryAction = c => {
+    let time = state.time
+    state.time = time + 1
+    let [ySum, yStr] = multiRoll(c.skill.level)
+    let [nSum, nStr] = multiRoll(c.skill.level)
+    state.attempt = {
+      time: time,
+      action: {
+        player: c.player,
+        skill: c.skill,
+        name: c.name,
+      },
+      result: {
+        diceFor: yStr,
+        diceAgainst: nStr,
+        success: ySum > nSum,
+      }
+    }
+    sendUpdate(state)
+  }
+
+  let doFinishAction = c => {
+    if (state.attempt) {
+      state.attempt.text = c.text
+      state.events.push(state.attempt)
+      state.attempt = null
+    }
+    sendUpdate(state)
+  }
+
   let makeComponent = (_data, up) => {
     let onCommand = c => {
       if (c.do === 'addplayer') {
-        state.players.push({
-          name: c.name,
-          skills: [
-            { name: 'fari ion', level: 1 },
-          ],
-        })
-        sendUpdate(state)
-      } else if (c.do  === 'action') {
-        let time = state.time
-        state.events.push({
-          time: time,
-          player: c.player,
-          skill: c.skill,
-          name: c.name,
-        })
-        state.time = time + 1
-        sendUpdate(state)
+        doAddPlayer(c)
+      } else if (c.do  === 'tryaction') {
+        doTryAction(c)
+      } else if (c.do === 'finishaction') {
+        doFinishAction(c)
       }
     }
 
@@ -210,7 +376,8 @@ function setup() {
   ui.addComponent(makePlayersList)
   ui.addComponent(makeNewPlayerBox)
   ui.addComponent(makeStoryBox)
-  ui.addComponent(makeDoSomethingBox)
+  ui.addComponent(makeMakeActionBox)
+  ui.addComponent(makeTryActionBox)
 
   return ui
 }
