a5467ed91d32cede111b6f75a36e477395463c24
Phil Housley <undeconstructed@gmail.com>
2022-05-03 03:29:23 +0100

    Somethings can be done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 
+const diceEmojis = '⚀⚁⚂⚃⚄⚅'
+const faceEmojis = '😭😣😕🙂😃'
+
 function getTemplate(selector) {
   let t = document.querySelector(selector).content.firstElementChild
   return () => {
@@ -33,7 +36,7 @@ function newUI(data) {
   }
 
   let sendCommand = (cmd) => {
-    console.log('command', cmd)
+    // console.log('command', cmd)
     for (let c of components) {
       c.onCommand && c.onCommand(cmd)
     }
@@ -44,7 +47,7 @@ function newUI(data) {
   }
 
   let onUpdate = u => {
-    console.log('rxu', JSON.stringify(u))
+    // console.log('rxu', JSON.stringify(u))
     state = u
 
     for (let c of components) {
@@ -80,7 +83,13 @@ function makePlayersList(_data, up) {
     list.replaceChildren()
     for (let pl of s.players) {
       let div = playerTmpl()
+      div.querySelector('.delete').addEventListener('click', e => {
+        if (confirm('Vere forigi?')) {
+          up.send({ do: 'deleteplayer', player: pl })
+        }
+      })
       div.querySelector('.name').textContent = pl.name
+      div.querySelector('.xp').textContent = pl.xp
       let skills = div.querySelector('.skills')
       for (let sk of pl.skills) {
         let sdiv = skillTmpl()
@@ -101,22 +110,25 @@ function makePlayersList(_data, up) {
   return { onCommand, onUpdate }
 }
 
-function makeStoryBox(_data, _up) {
+function makeStoryBox(_data, up) {
   let seen = 0
   let div = document.querySelector('.story')
   let list = div.querySelector('.list')
   let eventTmpl = getTemplate('#eventtemplate')
+
+  ;(() => {
+    let addButton = div.querySelector('.add')
+    addButton.addEventListener('click', e => {
+      up.send({ do: 'newevent' })
+    })
+  })()
 
   let onUpdate = s => {
     if (s.events.length > seen) {
       for (; seen < s.events.length; seen++) {
         let ev = s.events[seen]
         let ediv = eventTmpl()
-        if (ev.text) {
-          ediv.textContent = `Je ${ev.time}, ${ev.text}`
-        } else {
-          ediv.textContent = `Je ${ev.time}, ${ev.player.name}, faras ${ev.name}.`
-        }
+        ediv.textContent = ev.text
         list.append(ediv)
         ediv.scrollIntoView()
       }
@@ -124,6 +136,52 @@ function makeStoryBox(_data, _up) {
   }
 
   return { onUpdate }
+}
+
+function makeNewEventBox(_data, up) {
+  let div = document.querySelector('.newevent')
+  let form = div.querySelector('form')
+  let textBox = form.querySelector('[name=text]')
+
+  ;(() => {
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      let text = textBox.value
+      if (!text) {
+        return
+      }
+      doMake(text)
+      close()
+    })
+    form.querySelector('[name=cancel]').addEventListener('click', e => {
+      e.preventDefault()
+      close()
+    })
+  })()
+
+  let doMake = text => {
+    up.send({
+      do: 'insertevent',
+      text: text,
+    })
+  }
+
+  let setup = c => {
+    div.classList.add('open')
+  }
+
+  let close = () => {
+    div.classList.remove('open')
+    form.reset()
+  }
+
+  let onCommand = c => {
+    if (c.do === 'newevent') {
+      setup(c)
+    }
+  }
+
+  return { onCommand }
 }
 
 function makeNewPlayerBox(_data, up) {
@@ -206,7 +264,7 @@ function makeMakeActionBox(_data, up) {
 
   let setup = c => {
     state = c
-    let m = `Ago de ${state.player.name} per '${state.skill.name}' je nivelo ${state.skill.level}:`
+    let m = `${state.player.name} povas ${state.skill.name} (${state.skill.level}), ĉu li ankaŭ povas ...`
     form.querySelector('.status').textContent = m
     div.classList.add('open')
   }
@@ -230,43 +288,113 @@ function makeTryActionBox(_data, up) {
   let div = document.querySelector('.tryaction')
   let result = div.querySelector('.result')
   let form = div.querySelector('form')
+  let skillBox = form.querySelector('[name=skill]')
+  let nextBox = form.querySelector('[name=next]')
 
   ;(() => {
     form.addEventListener('submit', e => {
       e.preventDefault()
-      let next = form.querySelector('[name=next]').value
+      let next = nextBox.value
       if (!next) {
         return
       }
-      doFinish(next)
+      let skill = skillBox.value
+      doFinish(next, skill)
       close()
     })
   })()
 
-  let doFinish = (text) => {
+  let close = () => {
+    div.classList.remove('open')
+    form.reset()
+  }
+
+  let doFinish = (text, skill) => {
     up.send({
       do: 'finishaction',
       text: text,
+      skill: skill,
     })
+  }
+
+  let makeDiceString = ns => {
+    let s = ''
+    for (let n of ns) {
+      s += diceEmojis[n-1] + ' '
+    }
+    return s
+  }
+
+  let normalizeScore = n => {
+    // TODO - some sort of scaling
+    if (n < -2) n = -2
+    if (n > 2) n = 2
+    return n
+  }
+
+  let makeFaceString = n => {
+    let p = (n+2) * 2
+    return faceEmojis.substring(p, p+2)
+  }
+
+  let tweakAction = action => {
+    let space0 = action.indexOf(' ')
+    if (space0 === -1) {
+      space0 = action.length
+    }
+    let word0 = action.slice(0, space0)
+    if (word0.endsWith('i')) {
+      word0 = word0.slice(0 , -1) + 'as'
+      action = word0 + action.slice(space0)
+    } else if (word0.endsWith('o')) {
+      word0 = word0 + 'n'
+      action = 'faras ' + word0 + action.slice(space0)
+    }
+    return action
   }
 
   let onUpdate = s => {
     if (s.attempt) {
       let attempt = s.attempt
-      div.classList.add('open')
 
       let m = `${attempt.action.player.name} provas ${attempt.action.name}, per '${attempt.action.skill.name}' je nivelo ${attempt.action.skill.level}`
       result.querySelector('.status').textContent = m
 
-      result.querySelector('.for').textContent = attempt.result.diceFor
-      result.querySelector('.against').textContent = attempt.result.diceAgainst
+      let nscore = normalizeScore(attempt.result.success)
 
-      let next = form.querySelector('[name=next]')
-      if (attempt.result.success) {
-        next.value = `${attempt.action.player.name} sukcese faras ${attempt.action.name}, kaj ...`
+      result.querySelector('.for').textContent = makeDiceString(attempt.result.diceFor)
+      result.querySelector('.against').textContent = makeDiceString(attempt.result.diceAgainst)
+      result.querySelector('.success').textContent = makeFaceString(nscore)
+
+      let player = attempt.action.player
+      let action = attempt.action.name
+
+      let text = `${player.name} `
+
+      if (nscore == 2) {
+        text += `tute sukcese ${tweakAction(action)}`
+      } else if (nscore == 1) {
+        text += tweakAction(action)
+      } else if (nscore == 0) {
+        text += `iom sukcese ${tweakAction(action)}`
       } else {
-        next.value = `${attempt.action.player.name} sensukcese provas ${attempt.action.name}.`
+        if (nscore == -1) {
+          text += `sensukcese provas ${action}`
+        } else {
+          text += `tute fuŝas provon ${action}`
+        }
+        if (attempt.result.xp > 0) {
+          text += `, sed gajnas ${attempt.result.xp} spert-poentojn`
+        }
       }
+      if (attempt.result.newSkill) {
+        text += `, kaj gajnos novan kapablon`
+      }
+      text += ', kaj ...'
+      nextBox.value = text
+
+      div.classList.toggle('newskill', attempt.result.newSkill)
+      div.classList.add('open')
     } else {
       div.classList.remove('open')
     }
@@ -284,6 +412,19 @@ function makeKernel() {
     events: [],
   }
 
+  {
+    let playersS = localStorage.getItem('players')
+    if (playersS) {
+      state.players = JSON.parse(playersS)
+      // migrations
+      for (let pl of state.players) {
+        if (!pl.xp) {
+          pl.xp = 0
+        }
+      }
+    }
+  }
+
   let sendUpdate = () => {}
 
   let setUpdateFunction = f => {
@@ -293,29 +434,55 @@ function makeKernel() {
   let doAddPlayer = c => {
     state.players.push({
       name: c.name,
+      xp: 0,
       skills: [
         { name: 'fari ion', level: 1 },
       ],
     })
+    localStorage.setItem('players', JSON.stringify(state.players))
+    sendUpdate(state)
+  }
+
+  let doDeletePlayer = c => {
+    let i = state.players.findIndex(e => e == c.player)
+    if (i >= 0) {
+      state.players.splice(i, 1)
+      localStorage.setItem('players', JSON.stringify(state.players))
+      sendUpdate(state)
+    }
+  }
+
+  let doInsertEvent = c => {
+    let time = state.time
+    state.time = time + 1
+
+    state.events.push({
+      time: time,
+      text: c.text,
+    })
+
     sendUpdate(state)
   }
 
   let multiRoll = n => {
-    let sum = 0
-    let str = ''
+    let rolls = []
     for (let i = 0; i < n; i++) {
       let v = roll(6)
-      sum += v
-      str += v + ' '
+      rolls.push(v)
     }
-    return [sum, str]
+    return rolls
   }
 
   let doTryAction = c => {
     let time = state.time
     state.time = time + 1
-    let [ySum, yStr] = multiRoll(c.skill.level)
-    let [nSum, nStr] = multiRoll(c.skill.level)
+
+    let yRolls = multiRoll(c.skill.level)
+    let nRolls = multiRoll(c.skill.level)
+    let ySum = yRolls.reduce((accum, value) => accum + value, 0)
+    let nSum = nRolls.reduce((accum, value) => accum + value, 0)
+    let allSix = yRolls.every(value => value == 6)
+
     state.attempt = {
       time: time,
       action: {
@@ -324,20 +491,36 @@ function makeKernel() {
         name: c.name,
       },
       result: {
-        diceFor: yStr,
-        diceAgainst: nStr,
-        success: ySum > nSum,
+        diceFor: yRolls,
+        diceAgainst: nRolls,
+        success: ySum-nSum,
+        xp: nSum-ySum,
+        newSkill: allSix,
       }
     }
+
     sendUpdate(state)
   }
 
   let doFinishAction = c => {
-    if (state.attempt) {
-      state.attempt.text = c.text
-      state.events.push(state.attempt)
-      state.attempt = null
+    let attempt = state.attempt
+    if (attempt) {
+      attempt.text = c.text
+      state.events.push(attempt)
+      if (attempt.result.xp > 0) {
+        attempt.action.player.xp += attempt.result.xp
+        localStorage.setItem('players', JSON.stringify(state.players))
+      }
+      if (attempt.result.newSkill && c.skill) {
+        let newSkill = {
+          name: c.skill,
+          level: attempt.action.skill.level + 1
+        }
+        attempt.action.player.skills.push(newSkill)
+        localStorage.setItem('players', JSON.stringify(state.players))
+      }
     }
+    state.attempt = null
     sendUpdate(state)
   }
 
@@ -345,6 +528,10 @@ function makeKernel() {
     let onCommand = c => {
       if (c.do === 'addplayer') {
         doAddPlayer(c)
+      } else if (c.do === 'deleteplayer') {
+        doDeletePlayer(c)
+      } else if (c.do === 'insertevent') {
+        doInsertEvent(c)
       } else if (c.do  === 'tryaction') {
         doTryAction(c)
       } else if (c.do === 'finishaction') {
@@ -359,7 +546,11 @@ function makeKernel() {
     return { onCommand, onUpdate }
   }
 
-  return { setUpdateFunction, makeComponent }
+  let start = () => {
+    sendUpdate(state)
+  }
+
+  return { setUpdateFunction, makeComponent, start }
 }
 
 function setup() {
@@ -376,8 +567,11 @@ function setup() {
   ui.addComponent(makePlayersList)
   ui.addComponent(makeNewPlayerBox)
   ui.addComponent(makeStoryBox)
+  ui.addComponent(makeNewEventBox)
   ui.addComponent(makeMakeActionBox)
   ui.addComponent(makeTryActionBox)
+
+  server.start()
 
   return ui
 }
